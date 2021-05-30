41e431565f447e537171f356bf8539f393a83d09
Rene Del Carpio <delcarpio.rene@gmail.com>
2021-05-29 22:45:30 -0400

    se crearon los elementos de formulario y el tema donde estan todos los colores, se estructuro la pagina de registro de usuarios

--- src/components/RegistroUsuarios.js
+++ src/components/RegistroUsuarios.js
@@ -1,10 +1,58 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
+import {
+	Header,
+	Titulo,
+	ContenedorHeader,
+	ContenedorBotones,
+} from '../elements/Header';
+import Boton from '../elements/Boton';
+import {
+	Formulario,
+	Input,
+	ContenedorBoton,
+} from '../elements/ElementosDeFormulario';
+import { ReactComponent as SvgLogin } from '../images/registro.svg';
+import styled from 'styled-components';
+
+const Svg = styled(SvgLogin)`
+	width: 100%;
+	max-height: 6.25rem; /* 100px */
+	margin-bottom: 1.25rem; /* 20px */
+`;
 
 const RegistroUsuarios = () => {
 	return (
-		<div>
-			<h1>Registro usuarios</h1>
-		</div>
+		<>
+			<Helmet>
+				<title>Crear Cuenta</title>
+			</Helmet>
+
+			<Header>
+				<ContenedorHeader>
+					<Titulo>Crear Cuenta</Titulo>
+					<div>
+						<Boton to='/iniciar-sesion'>Iniciar Sesión</Boton>
+					</div>
+				</ContenedorHeader>
+			</Header>
+
+			<Formulario>
+				<Svg />
+				<Input type='email' name='email' placeholder='Correo Electrónico' />
+				<Input type='password' name='password' placeholder='Contraseña' />
+				<Input
+					type='password'
+					name='password2'
+					placeholder='Repetir la Contraseña'
+				/>
+				<ContenedorBoton>
+					<Boton as='button' primario type='submit'>
+						Crear Cuenta
+					</Boton>
+				</ContenedorBoton>
+			</Formulario>
+		</>
 	);
 };
 
