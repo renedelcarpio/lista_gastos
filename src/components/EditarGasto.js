e505c40531c3859e338c56f0a41664be63fd9799
Rene Del Carpio <delcarpio.rene@gmail.com>
2021-06-03 12:52:07 -0400

    se creo el hook useObtenerGasto donde obtuvimos el gasto por su id y este hook se lo pasamos al componente EditarGasto

--- src/components/EditarGasto.js
+++ src/components/EditarGasto.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { Header, Titulo } from '../elements/Header';
+import { Helmet } from 'react-helmet';
+import BtnRegresar from '../elements/BtnRegresar';
+import BarraTotalGastado from './BarraTotalGastado';
+import FormularioGasto from './FormularioGasto';
+import { useParams } from 'react-router-dom';
+import useObtenerGasto from '../hooks/useObtenerGasto';
 
 const EditarGasto = () => {
+	const { id } = useParams();
+	const [gasto] = useObtenerGasto(id);
+
+	console.log(gasto);
+
 	return (
-		<div>
-			<h1>Editar gasto</h1>
-		</div>
+		<>
+			<Helmet>
+				<title>Editar Gasto</title>
+			</Helmet>
+
+			<Header>
+				<BtnRegresar />
+				<Titulo>Editar Gasto</Titulo>
+			</Header>
+			<FormularioGasto />
+			<BarraTotalGastado />
+		</>
 	);
 };
 
